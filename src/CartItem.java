4b42c53ab2d9cd4282dc8a4ebe57a6a2bb41141c
crystalynliu <crystalynliu@163.com>
2016-03-06 08:25:21 +0800

    add CartItem.class

---
+++ src/CartItem.java
@@ -0,0 +1,20 @@
+public class CartItem {
+    private Item item;
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+}
