5a42a80bef52499ff2771ba3fd56979559efcefc
crystalynliu <crystalynliu@163.com>
2016-03-07 00:27:53 +0800

    add third test case to PosTest

--- Test/PosTest.java
+++ Test/PosTest.java
@@ -68,4 +68,31 @@ public class PosTest {
             assertThat(ReceiptItems.get(i).getSubtotal(),is(expectations[i]));
         }
     }
+
+    @Test
+    public void print_correct_text_when_input_receiptItems(){
+        List<ReceiptItem> receiptItems = new ArrayList<ReceiptItem>();
+        double[][] data = {{},{5.00,12.00,3.00},{},{2.00,30.00,0.00},{},{3.00,9.00,4.50}};
+        for(int i =0;i<data.length;i++){
+            i++;
+            CartItem cartItem = new CartItem(allItems.get(i),data[i][0]);
+            ReceiptItem receiptItem = new ReceiptItem();
+            receiptItem.setCartItem(cartItem);
+            receiptItem.setSubtotal(data[i][1]);
+            receiptItem.setSubSave(data[i][2]);
+            receiptItems.add(receiptItem);
+        }
+
+        String expectation="***<没钱赚商店>收据***\n" +
+                "名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n" +
+                "名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n" +
+                "名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n" +
+                "----------------------\n" +
+                "总计：51.00(元)\n"+
+                "节省：7.50(元)\n" +
+                "**********************";
+
+        String result = pos.printReceipt(receiptItems);
+        assertThat(result,is(expectation));
+    }
 }
