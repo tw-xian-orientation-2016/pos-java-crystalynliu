1ae7db53a23e4a7070c6d3cf0c363596ed0b289c
crystalynliu <crystalynliu@163.com>
2016-03-06 08:14:29 +0800

    add Item.java

---
+++ src/Item.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item {
+    private String barcode;
+    private String name;
+    private String unit;
+    private double price;
+
+    public Item(String barcode, String name, String unit, double price) {
+        this.barcode = barcode;
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Item> loadAllItems() {
+        List<Item> allItems = new ArrayList<Item>();
+        Item item = new Item("ITEM000000", "可口可乐", "瓶", 3.00);
+        allItems.add(item);
+        item = new Item("ITEM000001", "雪碧", "瓶", 3.00);
+        allItems.add(item);
+        item = new Item("ITEM000002", "雪碧", "瓶", 3.00);
+        allItems.add(item);
+        item = new Item("ITEM000003", "荔枝", "斤", 3.00);
+        allItems.add(item);
+        item = new Item("ITEM000004", "电池", "个", 2.00);
+        allItems.add(item);
+        item = new Item("ITEM000005", "方便面", "袋", 4.50);
+        allItems.add(item);
+
+        return allItems;
+    }
+}
