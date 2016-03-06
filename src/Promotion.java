9c51f03b43f124ae086dbd8ea76f9fb988ebd355
crystalynliu <crystalynliu@163.com>
2016-03-06 08:22:43 +0800

    add Promotions

---
+++ src/Promotion.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Promotion {
+    private String type;
+    private String[] barcode;
+
+    public Promotion(String type,String[] barcode){
+        this.type = type;
+        this.barcode = barcode;
+    }
+
+    public void setBarcode(String[] barcode) {
+        this.barcode = barcode;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String[] getBarcode() {
+        return barcode;
+    }
+
+    public List<Promotion> loadPromotions(){
+        List<Promotion> promotions = new ArrayList<Promotion>();
+        String[] barcodes = {"ITEM000000","ITEM000001","ITEM000005"};
+        Promotion promotion = new Promotion("BUY_TWO_GET_ONE_FREE",barcodes);
+        promotions.add(promotion);
+        return promotions;
+    }
+}
